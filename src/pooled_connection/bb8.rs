52a26d6bef5acd679f5ce948714715f217cab57b
Georg Semmler <github@weiznich.de>
2022-10-18 16:04:16 +0200

    Minor documentation improvements

--- src/pooled_connection/bb8.rs
+++ src/pooled_connection/bb8.rs
@@ -6,6 +6,7 @@
 //! use futures::FutureExt;
 //! use diesel_async::pooled_connection::AsyncDieselConnectionManager;
 //! use diesel_async::pooled_connection::bb8::Pool;
+//! use diesel_async::RunQueryDsl;
 //!
 //! # #[tokio::main(flavor = "current_thread")]
 //! # async fn main() {
@@ -15,7 +16,7 @@
 //! # #[cfg(feature = "postgres")]
 //! # fn get_config() -> AsyncDieselConnectionManager<diesel_async::AsyncPgConnection> {
 //! #     let db_url = database_url_from_env("PG_DATABASE_URL");
-//!   let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(db_url);
+//! let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(db_url);
 //! #     config
 //! #  }
 //! #
@@ -29,14 +30,14 @@
 //! # async fn run_test() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
 //! #     use schema::users::dsl::*;
 //! #     let config = get_config();
-//!   let pool = Pool::builder().build(config).await?;
-//!   let mut conn = pool.get().await?;
+//! let pool = Pool::builder().build(config).await?;
+//! let mut conn = pool.get().await?;
 //! # conn.begin_test_transaction();
 //! # clear_tables(&mut conn).await;
 //! # create_tables(&mut conn).await;
-//! #[cfg(feature = "mysql")]
+//! # #[cfg(feature = "mysql")]
 //! # conn.begin_test_transaction();
-//!   let res = users.load::<(i32, String)>(&mut conn).await?;
+//! let res = users.load::<(i32, String)>(&mut conn).await?;
 //! #     Ok(())
 //! # }
 //! ```
