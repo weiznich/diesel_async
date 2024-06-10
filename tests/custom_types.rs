65bcd7a9ee2bce85112a3226ef56b0d1d10326a9
dullbananas <dull.bananas0@gmail.com>
2024-06-09 19:12:16 -0700

    Update custom_types.rs

--- tests/custom_types.rs
+++ tests/custom_types.rs
@@ -86,7 +86,7 @@ async fn custom_types_round_trip() {
     // Try encoding arrays to test type metadata lookup
     let selected_data = (vec![MyEnum::Foo, MyEnum::Bar], vec![0i32], vec![vec![MyEnum::Foo]]);
     let selected = select(
-        selected_data.as_sql::<(Array<MyType>, Array<Integer>, Array<Array<MyType>>)>(),
+        selected_data.clone().into_sql::<(Array<MyType>, Array<Integer>, Array<Array<MyType>>)>(),
     )
     .get_result::<(Vec<MyEnum>, Vec<i32>, Vec<Vec<MyEnum>>)>(connection)
     .await
