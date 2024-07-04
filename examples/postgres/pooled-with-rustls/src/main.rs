144b488a6d1fb966734e3c1c6d21e5ff0e012102
Brobb954 <119805322+Brobb954@users.noreply.github.com>
2024-07-04 10:21:17 -0600

    Update rustls exmaple for work with newest updates to the crate. Also README section added to point out the need for this example

--- examples/postgres/pooled-with-rustls/src/main.rs
+++ examples/postgres/pooled-with-rustls/src/main.rs
@@ -63,7 +63,6 @@ fn establish_connection(config: &str) -> BoxFuture<ConnectionResult<AsyncPgConne
 fn root_certs() -> rustls::RootCertStore {
     let mut roots = rustls::RootCertStore::empty();
     let certs = rustls_native_certs::load_native_certs().expect("Certs not loadable!");
-    let certs: Vec<_> = certs.into_iter().map(|cert| cert.0).collect();
-    roots.add_parsable_certificates(&certs);
+    roots.add_parsable_certificates(certs);
     roots
 }
